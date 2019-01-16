aa9a5d05eaa3d7a5eeb6a07d3d34f444508af08a
Dennis QU <qu.dennis@gmail.com>
2019-01-16 10:45:09 +0800

    push test

--- push.sh
+++ push.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
+hugo -d ../wisblue.github.io
 git add .
 echo "Enter commit message: "
 read commitMessage
 git commit -m "$commitMessage"
 git push
 
-cp -r public/* ../wisbue.github.io
-cd ../wisbue.github.io
+# cp -r public/* ../wisblue.github.io
+cd ../wisblue.github.io
 git add .
 git commit -m "$commitMessage"
 git push
